e8d37950708d7db05fa8596ca5bbcd35d8da961e
Jasper Haward <jasperhaward@gmail.com>
2023-08-31 22:53:29 +0100

    feat: broadcast DEV server

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  esbuild: {
-    define: {
-      this: "window",
-    },
+  server: {
+    host: "0.0.0.0",
   },
 });
