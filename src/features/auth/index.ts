acc3a89fa87f6881da5e604f24d5dae7b5b214d9
Jasper Haward <jasperhaward@gmail.com>
2023-09-10 17:57:42 +0100

    refactor: move Session type to types filer

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,5 +1,5 @@
 export * from "./AuthedRoute";
-export * from "./Session";
 export * from "./SessionContext";
+export * from "./types";
 export * from "./useSession";
 export * from "./useSessionContext";
