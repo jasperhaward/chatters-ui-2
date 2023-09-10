17931df5c64f9d92b875c3789e415eb63a1ed8f0
Jasper Haward <jasperhaward@gmail.com>
2023-09-10 17:38:06 +0100

    refactor: hook UseLocalStorage type

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,8 +1,11 @@
 import { useState } from "preact/hooks";
 
-export function useLocalStorage<T>(
-  key: string,
-): [T | null, (value: T | null) => void] {
+export type UseLocalStorage<T> = [
+  value: T | null,
+  setStoredValue: (value: T | null) => void
+];
+
+export function useLocalStorage<T>(key: string): UseLocalStorage<T> {
   const [value, setValue] = useState(initialiser);
 
   function initialiser() {
