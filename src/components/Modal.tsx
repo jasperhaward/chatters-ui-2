d9757b1449cf53e2db34af03c179d0dab9a13455
Jasper Haward <jasperhaward@gmail.com>
2023-09-17 17:16:00 +0100

    refactor: remove isOpen prop from <Modal />

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,22 +3,18 @@ import styles from "./Modal.module.scss";
 import { IconButton } from ".";
 
 export interface ModalProps {
-  isOpen: boolean;
   title: string;
   children: ComponentChildren;
   onClose: () => void;
 }
 
-export function Modal({ isOpen, title, children, onClose }: ModalProps) {
+export function Modal({ title, children, onClose }: ModalProps) {
   function stopPropagation(event: JSX.TargetedMouseEvent<HTMLDivElement>) {
     event.stopPropagation();
   }
 
   return (
-    <div
-      className={`${styles.modalCurtain} ${isOpen ? styles.open : ""}`}
-      onClick={onClose}
-    >
+    <div className={styles.modalCurtain} onClick={onClose}>
       <div className={styles.modal} onClick={stopPropagation}>
         <div className={styles.title}>
           <h3>{title}</h3>
