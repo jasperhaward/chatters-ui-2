7beb6d37038f8c88219118498ced3905146786a6
Jasper Haward <jasperhaward@gmail.com>
2023-09-17 16:20:15 +0100

    refactor: remove error message className prop

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,12 +1,9 @@
 import styles from "./ErrorMessage.module.scss";
 
 export interface ErrorMessageProps {
-  className?: string;
   children: string;
 }
 
-export function ErrorMessage({ className = "", children }: ErrorMessageProps) {
-  return (
-    <div className={`${styles.errorMessage} ${className}`}>{children}</div>
-  );
+export function ErrorMessage({ children }: ErrorMessageProps) {
+  return <div className={styles.errorMessage}>{children}</div>;
 }
