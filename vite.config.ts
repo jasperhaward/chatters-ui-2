01e3355b15ef7fb8aa84cc22cf5cd8a6e7e46043
Jasper Haward <jasperhaward@gmail.com>
2023-11-09 19:05:05 +0000

    feat: support for environment variable placeholders in local development and at docker container startup

--- vite.config.ts
+++ vite.config.ts
@@ -16,32 +16,67 @@ export default defineConfig({
 });
 
 /**
- * Replaces placeholders of the format `${ENVIRONMENT_VARIABLE_NAME}` with their
- * respective environment variables in local development. Uses Linux's `envsubt`
- * placeholder format as we use `envsubt` to inject environment variables into
- * the bundle at Docker image startup.
- * @param path file path to inject environment variables into
+ * Plugin to inject environment variables during local development and enable
+ * support for Linux's `envsubt` command on the bundle at docker container startup.
+ * @param path path to file containing placeholders
  */
 function envsubst(path: string): Plugin {
-  return {
-    name: "vite-plugin-envsubst",
-    apply: "serve",
-    transform(code, id) {
-      if (id.endsWith(path)) {
-        const placeholders = code.matchAll(/\${([0-9A-Z_]+)}/g);
+  let isBuild = false;
 
-        for (const [placeholder, variable] of placeholders) {
-          const value = process.env[variable];
+  /**
+   * Creates a separate chunk for the file which we will inject environment variables
+   * into at docker container startup, reducing the chance of replacing non-placeholders
+   * or breaking the main JavaScript bundle.
+   */
+  function manualChunks(id: string) {
+    if (id.endsWith(path)) {
+      // use the modules's filename as the chunk name
+      const filename = id.substring(
+        id.lastIndexOf("/") + 1,
+        id.lastIndexOf(".")
+      );
 
-          if (!value) {
-            throw new Error(`Environment variable '${variable}' not found.`);
-          }
+      return filename;
+    }
+  }
 
-          code = code.replace(placeholder, value);
+  /**
+   * Replaces placeholders of the format `${ENVIRONMENT_VARIABLE_NAME}` with their
+   * respective environment variables in local development.
+   */
+  function transform(code: string, id: string) {
+    if (!isBuild && id.endsWith(path)) {
+      const placeholders = code.matchAll(/\${([0-9A-Z_]+)}/g);
+
+      for (const [placeholder, variable] of placeholders) {
+        const value = process.env[variable];
+
+        if (!value) {
+          throw new Error(`Environment variable '${variable}' not found.`);
         }
 
-        return { code };
+        code = code.replace(placeholder, value);
       }
+
+      return { code };
+    }
+  }
+
+  return {
+    name: "vite-plugin-envsubst",
+    config(_, env) {
+      isBuild = env.command === "build";
+
+      return {
+        build: {
+          rollupOptions: {
+            output: {
+              manualChunks,
+            },
+          },
+        },
+      };
     },
+    transform,
   };
 }
